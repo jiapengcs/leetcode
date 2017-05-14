f80d11b83439315f207ed2d978befcc338709e66
Jiapeng <741266933@qq.com>
2017-05-14 20:57:36 +0800

    No_1 accepted

--- src/easy/No_1.java
+++ src/easy/No_1.java
@@ -5,9 +5,20 @@ import java.util.HashMap;
 
 /**
  * Created by Jiapeng on 2017/5/14.
+ *
+ * 1. Two Sum
+ *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
  */
 public class No_1 {
-    public static int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
         int[] answer = new int[2];
         HashMap<Integer, Integer> buf = new HashMap();
         for (int i = 0; i < nums.length; i++) {
@@ -20,11 +31,5 @@ public class No_1 {
             }
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
-        int target = 6;
-        System.out.println(Arrays.toString(twoSum(nums, target)));
     }
 }
