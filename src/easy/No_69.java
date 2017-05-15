49b3bed0211f8ca836f9271584cd15a915673a37
Jiapeng <741266933@qq.com>
2017-05-15 21:12:56 +0800

    No_69 AC

---
+++ src/easy/No_69.java
@@ -0,0 +1,26 @@
+package easy;
+
+/**
+ * Created by Jiapeng on 2017/5/15.
+ *
+ * 69. Sqrt(x)
+ *
+ * Implement int sqrt(int x).
+ * Compute and return the square root of x.
+ */
+public class No_69 {
+    public static int mySqrt(int x) {
+        double answer = x, tmp = 0;
+        while (Math.abs(answer - tmp) > 0.000001) {
+            tmp = answer;
+            answer = (answer + x / answer) / 2;
+        }
+        return (int) answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(2));
+        System.out.println(mySqrt(9801));
+        System.out.println(mySqrt(2147395599));
+    }
+}
