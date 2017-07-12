0376c931d652bfe5324853480f2a05ac7617cd7c
Jiapeng <741266933@qq.com>
2017-07-12 10:33:52 +0800

    problem 35, 66, 88 solved

--- src/easy/No_66.java
+++ src/easy/No_66.java
@@ -4,6 +4,17 @@ import java.util.Arrays;
 
 /**
  * Created by Jiapeng on 2017/7/11.
+ *
+ * 66. Plus One
+ *
+ * Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+ *
+ * You may assume the integer do not contain any leading zero, except the number 0 itself.
+ *
+ * The digits are stored such that the most significant digit is at the head of the list.
+ *
+ * 思路：
+ *  给定一个以非空数组表示的非负整数，对这个整数加一
  */
 public class No_66 {
     public static int[] plusOne(int[] digits) {
