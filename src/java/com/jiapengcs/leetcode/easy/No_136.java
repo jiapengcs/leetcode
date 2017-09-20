f9a8d5a7fdda60b228ffe212093d622895bcfba3
Jiapeng <741266933@qq.com>
2017-09-20 09:16:31 +0800

    136, 141 solved

--- src/java/com/jiapengcs/leetcode/easy/No_136.java
+++ src/java/com/jiapengcs/leetcode/easy/No_136.java
@@ -1,5 +1,10 @@
 package com.jiapengcs.leetcode.easy;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by Jiapeng on 2017/9/17.
  *
@@ -7,7 +12,10 @@ package com.jiapengcs.leetcode.easy;
  *
  * Given an array of integers, every element appears twice except for one. Find that single one.
  *
- * 分析：按位异或，两个相同的数会相互抵消成为0.
+ * 分析：给定一个整数数组，只有一个数出现一次，其余所有数都出现两次。找出这个落单的数。
+ * 方法一：按位异或，两个相同的数会相互抵消成为0.
+ * 方法二：Hash表记录每个数出现的次数。
+ * 方法三：遍历将数加入集合，计算：所有数的累加 - 2 * 集合的累加和。
  */
 public class No_136 {
     // TC: O(n), SC: O(1)
@@ -19,7 +27,29 @@ public class No_136 {
         return ans;
     }
 
+    // TC: O(n), SC: O(n)
+    public static int singleNumber2(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : nums)
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        for (int k : map.keySet())
+            if (map.get(k) == 1) return k;
+        return 0;
+    }
+
+    // TC: O(n), SC: O(n)
+    public static int singleNumber3(int[] nums) {
+        int sum = 0, tmp = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int x : nums) {
+            sum += x;
+            set.add(x);
+        }
+        for (int y : set) tmp += y;
+        return sum - 2 * tmp;
+    }
+
     public static void main(String[] args) {
-        System.out.println(singleNumber(new int[] {1, 1, 2, 3, 3}));
+        System.out.println(singleNumber3(new int[] {1, 1, 2, 3, 3}));
     }
 }
--- src/java/com/jiapengcs/leetcode/easy/No_141.java
+++ src/java/com/jiapengcs/leetcode/easy/No_141.java
@@ -2,20 +2,42 @@ package com.jiapengcs.leetcode.easy;
 
 import com.jiapengcs.leetcode.util.ListNode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Jiapeng on 2017/9/17.
+ *
+ * 141. Linked List Cycle
+ *
+ * Given a linked list, determine if it has a cycle in it.
+ *
+ * Follow up:
+ * Can you solve it without using extra space?
+ *
+ * 分析：判断单链表是否存在环。
+ * 方法一：使用快慢指针，快指针每次向后移动两个结点，慢指针每次向后移动一个结点，如果快慢指针相遇则说明存在环。
+ * 方法二：将遍历过的结点加入HashSet，如果遍历到相同的结点则说明存在环。
  */
 public class No_141 {
-    public static boolean hasCycle1(ListNode head) {
+    public static boolean hasCycle(ListNode head) {
         ListNode fast = head, slow = head;
-        while (fast != null && slow != null) {
-            if (fast.next != null) {
-                fast = fast.next.next;
-            } else {
-                return false;
-            }
+        while (fast != null) {
+            if (fast.next == null) return false;
+            fast = fast.next.next;
             slow = slow.next;
             if (fast == slow) return true;
+        }
+        return false;
+    }
+
+    public static boolean hasCycle2(ListNode head) {
+        ListNode node = head;
+        Set<ListNode> set = new HashSet<>();
+        while (node != null) {
+            if (set.contains(node)) return true;
+            else set.add(node);
+            node = node.next;
         }
         return false;
     }
@@ -27,6 +49,6 @@ public class No_141 {
         node1.next = node2;
         node2.next = node3;
         node3.next = node2;
-        System.out.println(hasCycle1(node1));
+        System.out.println(hasCycle(node1));
     }
 }
