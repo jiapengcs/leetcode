1e748a25f29dddcf4a295fa140a83879b52a5cb2
Jiapeng <741266933@qq.com>
2017-09-26 16:00:06 +0800

    111 solved

--- src/java/com/jiapengcs/leetcode/easy/No_111.java
+++ src/java/com/jiapengcs/leetcode/easy/No_111.java
@@ -13,13 +13,23 @@ import java.util.Queue;
  * Given a binary tree, find its minimum depth.
  * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
  *
- * 分析：求二叉树的最小深度
- * 方法一：层次遍历，记录层数，找到第一个叶子结点，此时的层数即最小深度。
- * 方法二：递归先序遍历每个结点，返回其左右子树中较小的高度。注意如果有子树为空，则应该返回另一棵子树的高度，而不是0.
+ * 分析：求二叉树的最小深度（与104题对应）
+ * 方法一：递归先序遍历每个结点，返回其左右子树中较小的高度。注意如果有子树为空，则应该返回另一棵子树的高度，而不是0.
+ * 方法二：层次遍历，记录层数，找到第一个叶子结点，此时的层数即最小深度。
  */
 public class No_111 {
+    // TC: O(n), SC: O(1)
+    public static int minDepth1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = minDepth1(root.left);
+        int right = minDepth1(root.right);
+        return (left == 0 || right == 0) ? (left + right + 1) : Math.min(left, right) + 1;
+    }
+
     // TC: O(n), SC: O(n)
-    public static int minDepth(TreeNode root) {
+    public static int minDepth2(TreeNode root) {
         if (root == null) {
             return 0;
         }
@@ -43,16 +53,6 @@ public class No_111 {
             }
         }
         return level;
-    }
-
-    // TC: O(n), SC: O(1)
-    public static int minDepth2(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int left = minDepth2(root.left);
-        int right = minDepth2(root.right);
-        return (left == 0 || right == 0) ? (left + right + 1) : Math.min(left, right) + 1;
     }
 
     public static void main(String[] args) {
