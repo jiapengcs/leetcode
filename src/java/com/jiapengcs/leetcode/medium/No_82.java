46296fa52a9c9194db7ceeeaa257453053d6b560
Jiapeng <741266933@qq.com>
2017-10-24 21:52:19 +0800

    solved

--- src/java/com/jiapengcs/leetcode/medium/No_82.java
+++ src/java/com/jiapengcs/leetcode/medium/No_82.java
@@ -14,15 +14,42 @@ import com.jiapengcs.leetcode.util.ListNode;
  * Given 1->2->3->3->4->4->5, return 1->2->5.
  * Given 1->1->1->2->3, return 2->3.
  *
- * 分析：83题的变种。
+ * 分析：83题的变种。如果head结点的值与后面的结点相同，则跳过这些值相同的结点，然后对后面的结点进行递归操作。
  */
 public class No_82 {
+    // TC: O(n), SC: O(1)
     public static ListNode deleteDuplicates(ListNode head) {
-        return null;
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = head.next;
+        while (p != null && p.val == head.val) {
+            p = p.next;
+        }
+        if (p != head.next) {  // duplicates found
+            return deleteDuplicates(p);
+        }
+        head.next = deleteDuplicates(head.next);
+        return head;
+    }
+
+    // TC: O(n), SC: O(1)
+    public static ListNode deleteDuplicates1(ListNode head) {
+        while (head != null && head.next != null && head.val == head.next.val) {
+            while (head.next != null && head.val == head.next.val) {
+                head = head.next;
+            }
+            head = head.next;
+        }
+        if (head != null) {
+            head.next = deleteDuplicates(head.next);
+        }
+        return head;
     }
 
     public static void main(String[] args) {
-        ListNode head = ListNode.link(new int[]{1, 2, 3, 3, 4, 4, 5});
+        ListNode head = ListNode.link(new int[]{1, 1, 2, 3});
+        head = deleteDuplicates(head);
         ListNode.print(head);
     }
 }
