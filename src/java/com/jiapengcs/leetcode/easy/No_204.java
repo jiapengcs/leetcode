5c934fd7ac9369ee8a8628ec2c7d03335dc12aba
Jiapeng <741266933@qq.com>
2017-12-30 00:50:24 +0800

    solved

--- src/java/com/jiapengcs/leetcode/easy/No_204.java
+++ src/java/com/jiapengcs/leetcode/easy/No_204.java
@@ -3,41 +3,64 @@ package com.jiapengcs.leetcode.easy;
 /**
  * @author Jiapeng
  * @version V1.0
- * date: 17-12-27
+ * date: 2017/12/29
  *
  * 204. Count Primes
  *
  * Description:
  * Count the number of prime numbers less than a non-negative number, n.
+ *
+ * 分析：
+ * 用数组isPrime[]记录n个数是否为素数，如果i为素数，则从i*i开始，将i*i + m*i标记为非素数。
+ * 改进：这n个数有一半是偶数，可以直接排除；i*i + m*i的标记过程简化为i*i + 2m*i，因为m为奇数时(m+1)*i可以被2整除。
  */
 public class No_204 {
-
+    // TC: O(nlog(logn)), SC: O(n)
     public static int countPrimes(int n) {
-        int count = 0;
-        for (int i = 1; i < n; i++) {
-            if (isPrime(i)) {
-                count++;
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
-        return count;
+        int ans = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                ans++;
+            }
+        }
+        return ans;
     }
 
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
+    // TC: O(nlog(logn)), SC: O(n)
+    public static int countPrimes2(int n) {
+        if (n < 3) {
+            return 0;
         }
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                return false;
+        boolean[] buf = new boolean[n];
+        int ans = n / 2;
+        for (int i = 3; i * i < n; i += 2) {
+            if (!buf[i]) {
+                for (int j = i * i; j < n; j += 2 * i) {
+                    if (!buf[j]) {
+                        --ans;
+                        buf[j] = true;
+                    }
+                }
             }
         }
-        return true;
+        return ans;
     }
 
     public static void main(String[] args) {
-        System.out.println(countPrimes(100));
-        System.out.println(countPrimes(1000));
-        System.out.println(countPrimes(10000));
-        System.out.println(countPrimes(1500000));
+        System.out.println(countPrimes2(2));
+        System.out.println(countPrimes2(100));
+        System.out.println(countPrimes2(1000));
+        System.out.println(countPrimes2(10000000));
     }
 }
